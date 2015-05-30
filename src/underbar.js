251544a7017c0dcdd9e4f52a1f48667fea9ffdde
Owen Dismuke <owen.dismuke@gmail.com>
2015-05-30 03:18:24 -0700

    Finished _.filter and _.reduce

--- src/underbar.js
+++ src/underbar.js
@@ -79,12 +79,24 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    
+    _.each(collection, function(item){
+      if (test(item)) {
+       result.push(item);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function (item){
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
